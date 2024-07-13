e0a5bafff891679c9d492e910b912747edc0cfc4
ibrahim13ind <ibrahimakmalafif@gmail.com>
2024-07-13 19:52:41 +0700

    kode pertama

---
+++ ko2_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char teks [20];
+  int x;
+  cout<<"masukkan sebuah kata      : ";  cin>>teks;
+  cout<< "masukkan sebuah angka    : ";  cin>>x;
+  cout<<teks<<endl;     //bisa ditulis dengan cout<<x<<'\n';
+  return 0;
+} 
