5651b00642a4d34a925b402bf81be0ae8712ff2e
ibrahim13ind <ibrahimakmalafif@gmail.com>
2024-07-13 20:02:19 +0700

    upload kode kedua

---
+++ kode_pro_2_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int A[5]
+    for (int  c=0 c<5;c++) {
+    a[c] = c*10;
+    }
+     for (int  c=0 c<5;c++) 
+     cout<<A[5]<<endl;  
+}
+return 0;
+}
