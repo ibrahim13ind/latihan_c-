099e03caf560fb5025b68a086fdb7d771114ee9c
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-14 19:51:52 +0700

    kode ke 7

---
+++ kode_ program_2-8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){ 
+    int x; 
+    //menampilkan nilai x sebelum 
+    //dilakukan pemasukan nilai (assignment)
+     cout <<"nilai x sebelum dilakukan assignment: "<<endl;
+     x=10  ;
+     cout <<"nilai x sesudah dilakukan assignment: "<<endl;
+     return 0;
+}
