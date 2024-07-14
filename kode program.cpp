d39de18666974823a400514cc00dbd251ea2bfad
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-14 19:00:15 +0700

    upload 5

---
+++ kode program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int A;  //variabel A adalah variabel global 
+          //karena dideklarasikandi luar fungsi
+//membuat fungsi utama
+int main () {
+    A=10;  // memasukkan (assign)   nilai kedalam 
+           //variabel 
+           cout<<"nilai A="<<A<<endl;
+      return 0;     
+
+}
