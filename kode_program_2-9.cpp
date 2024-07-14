8c3f31d0676d57722ac13ab31c9c5733685ec3a4
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-14 20:20:18 +0700

    upload ke 8

---
+++ kode_program_2-9.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+int x=5;
+cout<<"nilai x sebelum dilakukan assignment:"<<x<<endl;
+x=10;
+cout<<"nilai x sesudah dilakukan assignmnt :"<<x<<endl;
+return 0;
+}
