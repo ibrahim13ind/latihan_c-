4a9807f190fcbd873df3bff0ac04ce6cbd5a4162
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-14 19:38:13 +0700

    kode ke 6

---
+++ kode_program_2-7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//membuat fungsi utama
+int main(){
+    int A ;//variabel A bersifat lokal dan hanya dikenali
+           //oleh fungsi main()
+           A=10 ;     //memasukkakan (assign) nilai ke dalam 
+                      // variabel   
+        cout<<"nilai A  ="<<A<<endl;   
+        return 0  ;
+}  
