fa23534a5d2ad639d5a62ca4e2444c75b52815e4
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-14 07:11:09 +0700

    upload kode ke empat
    Pakai kode program 2-4 dengan cara kedua

---
+++ kode_program_2-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+const int MAX = 5 ;
+int main(){
+    int A[MAX] ;
+    for(int c=0;c<MAX; c++) {
+    a[c] = c*10;
+    }
+    for(int c=0;c<MAX;c++) {
+        cout<<A[C]<<endl
+    }
+    return 0;
+}
