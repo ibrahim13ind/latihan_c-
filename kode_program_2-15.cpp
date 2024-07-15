8a1f5d2b6096f918932799b014ca11dfea087583
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-15 16:28:10 +0700

    upload kode ke14

---
+++ kode_program_2-15.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std ;
+int main (){
+    //medeklarasikan variabel karakter dengan nilai'A'
+    char karakter= 'A';
+
+    //mendeklarasikan variabel teks dengan nilai "kata"
+    char* teks="kata";
+    char TEKS[10]="coba";
+    cout<<karakter<<endl;
+    cout<<teks<<endl;
+    cout<<TEKS<<endl;
+    return 0; 
+}
