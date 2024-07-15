7c2adb955774fdaf585fcd30b2365852a45b4a9b
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-15 16:14:34 +0700

    upload kode ke 13

---
+++ kode_program_2-14.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+ using namespace std ;
+ int main () {
+     double y; //mendelarasikan variabel y dengan tipe 
+               // data double 
+     y =222.134; // melakukan   assigment terhadap variabel 
+               // x
+   cout<<"nilai y="<<y;
+   cout<<endl;
+  return 0;                 
+ }
