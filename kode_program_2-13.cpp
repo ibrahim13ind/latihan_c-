bc8dd0aca2749aff0c8ff6172cc0a7feea982a19
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-15 16:01:22 +0700

    upload kode ke12

---
+++ kode_program_2-13.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std ;
+ int main (){
+     int x ;// mendelarasikan variabel x dengan tipe data int
+     x=3;//melakukan assigment terhadap variabel x
+     cout<<"nilai x="<<x;
+     cout<<endl;
+     return 0;
+     
+ }
