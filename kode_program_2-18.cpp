e0bc5281dbe820896d4addb9ff54775f27e38515
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-16 09:31:49 +0700

    upload kode ke 17

---
+++ kode_program_2-18.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    //mendeklarasikan variabel nama dengan tipe string dan 
+    //lebar 5 karakter  
+    char NAMA [5] ;
+    //mengisikan nilai kedalam variabel NAMA
+    NAMA ="Budi"
+    
+    cout<<"karakter ke-0: "<<NAMA[0]<<endl;
+    cout<<"karakter ke-1: "<<NAMA[1]<<endl;
+    cout<<"karakter ke-2: "<<NAMA[2]<<endl;
+    cout<<"karakter ke-3: "<<NAMA[3]<<endl;
+    cout<<"karakter ke-4: "<<NAMA[4]<<endl;
+    return 0;
+    
+    }
