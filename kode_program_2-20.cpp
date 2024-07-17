4f6f328d624572dbf5b73ac0cbeb311f99183799
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-17 14:29:23 +0700

    upload kode ke 20

---
+++ kode_program_2-20.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std ;
+int main(){
+    char s1[7];
+    
+    char* s2;
+   // melakukan pengisian nilai terhadap variabel s1 dan s2 
+   s1="contoh";
+   s2="salin string";
+  //melakukan penyalinan stringdari variabel s2 ke variabel s1
+  strcpy(s1,s2, 4); 
+  //menampilkan nilai dari variabel s1 
+  cout<<s1<<endl;
+  return 0; 
+}
