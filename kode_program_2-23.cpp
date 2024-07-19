c83db4382397f05ca445beac079c14fd2a15a23e
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-19 16:23:28 +0700

    upload kode ke  23
    kode ini masih ada error

---
+++ kode_program_2-23.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main (){
+    
+    const char* s1;
+    const char* s2;
+    //melakukan pengisian nilai terhadap variabel s1 dan s2 
+    s1 = "nama saya adalah";
+    s2 = "zayd abdulah";
+    //menggabungkan 4 karakter dari variabel s2 ke variabel s2
+    strncat(str1,str2, 4) ;
+    //menampilkan nilai dari variabel s1 setelah dilakukan penggabungan
+    cout<<s1<<endl ;
+    return 0;
+}
