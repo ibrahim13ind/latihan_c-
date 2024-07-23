726c5738e76d4699a6fa368659e8977a73d3a2a9
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-23 20:34:50 +0700

    upload kode ke 25

--- kode_program_2-25.cpp
+++ kode_program_2-25.cpp
@@ -1,13 +1,26 @@
-#include <iostream>
-#include <cstring> 
+#include <iostream> 
+#include <cstring>
 using namespace std ;
-int main
-    char* S ="c++ adalah segalanya bagiku";
-    char* PStr ; 
+int main(){
+    const char* s1;
+    const char* s2;
+    //melakukan pengisian nilai yang sama 
+    //terhadap variabel s1 dan s2 
+    s1="coba";
+    s2="coba";
 
-    //menampilakan pencarian karakter '+' didalam variabel s
-    PStr =strchr (s, '+') ;
-    //menampilkan nilai dari variabel PStr 
-    cout<<PStr<<endl;
+    //menampilkan hasil perbandingan string dari variabel s1 dan s2
+    cout<<"hasil perbadingan : " <<strcmp(s1, s2) <<endl;
+
+    //melakukan perubahan nilai terhadap variabel s1
+    s1="coba";
+
+    //menampilkan  kembali hasil perbadingan string dari variabel  s1 dan s2
+    cout<<"hasil perbadingan : "<<strcmp (s1, s2) <<endl;
+    //melakukan perubahan nilai terhadap variabel s2 
+    s2 ="coba";
+
+    //melakukan kembali hasil perbandingan string  dari variabel s1 dan s2
+    cout<<"hasil perbadingan : " <<strcmp (s1, s2)<<endl;
     return 0;
 }
