cb31d942da8654291c1bd7bd8820dc9637a3c0cb
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-24 13:59:02 +0700

    uplad kode ke 29
    belum bisa

---
+++ kode_program_2-29.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring> 
+ 
+ using namespace std ;
+
+int main (){
+    char* S="CONTOH MENGECILKAN HURUF";
+    char* hasil;
+    
+    //mengubah string dari variabel S menjadi huruf kecil 
+    hasil = strlwr (s) ;
+
+    //menampilkan nilai dari variabel hasil 
+    cout<< hasil<<endl; 
+
+    return 0;
+}
