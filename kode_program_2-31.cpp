49c7e0d9c6b8dd600eb1c02947d925868ed9e4b9
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-24 14:00:28 +0700

    upload kode ke 31

---
+++ kode_program_2-31.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring> 
+using namespace std ;
+int main  (){
+    char S [20];
+    cout<<"masukkan nama anda: ";
+    //meminta input dari user dan menyimpan nilainya kedalam variabel S
+    cin>>S;
+
+    //menampilkan kembali nama yang telah dimasukkan
+    cout<<"hai "<<S<<" apa kabarmu"<<endl;
+    return 0;
+}
