ceff12ffcd40575f42213a85f8fc22952f4f6fe2
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-25 14:56:29 +0700

    upload kode ke 37

---
+++ kode_program_3-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std ;
+int main(){
+    //mendeklarasikan variabel x (diisi nilai 10) dan y (diisi nilai 3)
+    int x = 10 ,y = 3;
+    //mendeklarasikan variabel z sebagai penampung nilai hasil operasi
+    int z; 
+
+    //melakukan operasi pejumlahan 
+    z = x + y;
+
+    //menampilkan hasil penjumlahan
+    cout<<x<<" + "<<y<<" = "<<z;
+    return 0;
+}
