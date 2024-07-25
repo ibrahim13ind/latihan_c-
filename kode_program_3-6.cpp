65082777ec6ac90a5ee9f45476b9d574ab62d271
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-25 15:11:20 +0700

    upload kode ke 9

---
+++ kode_program_3-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //mendeklarasikan variabel x (diisi nilai 10) dan y (diisi nilai 3)
+    int x =10, y = 3 ;
+    int z;//mendeklarasikan variabel z sebagai penampung nilai hasil operasi
+
+    //melakukan operasi pengurangan
+    z = x-y ;
+
+    //menampilkan hasil penjumlahan 
+    cout<<x<<" - "<<y<<" = "<<z;
+
+    return 0;
+}
