176740df43eeca2eb5ad6dd70e9b1b3c5ab07f8d
ibrahim <ibrahimakmalafif@gmail.com>
2024-07-25 15:20:41 +0700

    upload kode ke 40

---
+++ kode_program_3-7.cpp
@@ -0,0 +1,15 @@
+#include  <iostream> 
+
+using namespace std;
+
+int main(){
+    //mendeklarasikan variabel x(diisi 10) dan y(diisi 10)
+    int x = 10, y = 3;
+    int z; //mendklarasikan variabel z sebagai penampung nilai hasil operasi 
+
+    //melakukan operasi perkalian 
+    z = x * y;
+     //menampilkan hasil penjumlahan 
+     cout<<x<<" * "<<y<<" = "<<z;
+     return 0;
+    }
