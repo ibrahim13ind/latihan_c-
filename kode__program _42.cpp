e3f5076df13213ea0144fb88ebf8b3f1ed11b432
ibrahim <ibrahimakmalafif@gmail.com>
2024-08-16 18:10:41 +0700

    upload kode ke 42

---
+++ kode__program _42.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <cstring>      
+
+using namespace std;
+
+int main () {
+    cout<<"1   ||   1 = "<<(1  ||   1)<<endl;
+    cout<<"1   ||   0 = "<<(1  ||   0)<<endl;
+    cout<<"0   ||   0 = "<<(0  ||   0)<<endl;
+    cout<<"0   ||   1 = "<<(0  ||   1)<<endl;
+
+    return 0;
+}
